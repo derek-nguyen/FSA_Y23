68637d95d9125cfd5a2cbdebb50c0751e8c48a70
Derek Nguyen <derek.nguyen02@gmail.com>
2023-05-15 23:58:01 -0700

    added post request to retrieve user from db, will need to authenticate using jwt

--- Graded_Projects/juicebox/api/users.js
+++ Graded_Projects/juicebox/api/users.js
@@ -1,4 +1,8 @@
 // File to create sub-routes
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// console.log(process.env.JWT_SECRET);
 
 const express = require('express');
 const usersRouter = express.Router();
@@ -7,21 +11,48 @@ usersRouter.use((req, res, next) => {
     console.log("A request is being made to /users");
 
     next();
-  });
-  
+});
 
-const { getAllUsers } = require('../db')
+
+const { getAllUsers, getUserByUsername } = require('../db')
 
 usersRouter.get('/', async (req, res) => {
     const users = await getAllUsers();
-    
+
     res.send({
-        users:[users]
+        users: [users]
     });
-    
-    // res.send({
-    //     users:[]
-    // });
+});
+
+usersRouter.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
+    // console.log(username, password);
+
+    if (!username || !password) {
+        next({
+            name: "MissingCredentialsError",
+            message: "Please supply both a username and password"
+        });
+    }
+
+    try {
+        const user = await getUserByUsername(username);
+        console.log(user);
+
+        if (user && user.password == password) {
+            res.send({ message: "you're logged in" });
+        
+        } else {
+            next({
+                name: 'IncorrectCredentialsError',
+                message: 'Username or password is incorrect'
+            });
+        }
+
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
 });
 
 module.exports = usersRouter;
--- Graded_Projects/juicebox/db/index.js
+++ Graded_Projects/juicebox/db/index.js
@@ -313,6 +313,20 @@ async function getAllTags() {
     }
 }
 
+async function getUserByUsername(username) {
+    try {
+        const {rows:[user]} = await client.query(`
+            SELECT *
+            FROM users
+            WHERE username = $1;
+        `,[username]);
+
+        return user;
+    } catch (err) {
+        throw err;
+    }
+}
+
 module.exports = {
     client,
     getAllUsers,
@@ -327,4 +341,5 @@ module.exports = {
     getPostById,
     getPostsByTagName,
     getAllTags,
+    getUserByUsername,
 }
