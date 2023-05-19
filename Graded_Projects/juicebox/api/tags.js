3b9c66f7c179dce85ec313fb49eb7f5ce5bc8fe7
Derek Nguyen <derek.nguyen02@gmail.com>
2023-05-18 19:32:13 -0700

    added route to get posts owned by user and is active

--- Graded_Projects/juicebox/api/tags.js
+++ Graded_Projects/juicebox/api/tags.js
@@ -26,10 +26,14 @@ const {
 // });
 
 
-// curl http://localhost:3000/api/tags/\#happy/posts 
+// command to get all posts by tagName
+// curl http://localhost:3000/api/tags/%23happy/posts
+
+// command to test for get active posts + owned by user
+// curl http://localhost:3000/api/tags/%23happy/posts -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsYmVydCIsInBhc3N3b3JkIjoiYmVydGllOTkiLCJpZCI6MSwiaWF0IjoxNjg0NDUyODg3fQ.t6GjazdmrJOVgLdYOKmLU_Jy2Z-VGZTdjI290d_iSXg'
 tagsRouter.get('/:tagName/posts', async (req, res, next) => {
     const tagName = req.params.tagName;
-    const encodedTagName = encodeURIComponent(req.params.tagName)
+    // const encodedTagName = encodeURIComponent(req.params.tagName)
     // console.log(encodedTagName)
 
     
@@ -37,8 +41,18 @@ tagsRouter.get('/:tagName/posts', async (req, res, next) => {
         // use our method to get posts by tag name from the db
         // send out an object to the client {posts: // the posts}
 
-        const posts = await getPostsByTagName(encodedTagName)
-        res.send({ posts });
+        const AllTagPosts = await getPostsByTagName(tagName)
+        // console.log(AllTagPosts);
+        // console.log(req.user.id);
+
+        const tagPosts = AllTagPosts.filter(post => {
+            if (post.active && post.author.id === req.user.id) {
+                return true;
+            }
+
+        });
+
+        res.send({ tagPosts });
 
     } catch ({ name, message }) {
         next({ name, message });
